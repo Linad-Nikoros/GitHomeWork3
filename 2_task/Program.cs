31d2282161ba06f0f293aecbb0071575bc118e30
Linad-Nikoros <linad.nikoros@gmail.com>
2023-11-14 20:32:50 +0700

    add all file

---
+++ 2_task/Program.cs
@@ -0,0 +1,12 @@
+﻿
+
+int[] array1=new int[10] { 12 ,-47, -87, -98, 8 ,71 , 6, -52 ,21 , 7 };
+int neg = 0;
+for (int i=0;i<10;i++) 
+{
+if (array1[i]>0) neg = array1[i] * (-1);
+if (array1[i]<0) neg = array1[i] * (-1);
+Console.Write(neg+" ");
+}
+
+
---
+++ 3 task/Program.cs
@@ -0,0 +1,10 @@
+﻿ 
+Console.WriteLine("Введите число");
+int num1=Convert.ToInt32(Console.ReadLine());
+int[] array1=new int[10] { 12 ,-47, -87, -98, 8 ,71 , 6, -52 ,21 , 7 };
+
+for(int i = 0;i<10;i++)
+{
+if (array1[i] == num1) Console.WriteLine("такое число имеется в массиве");
+else Console.WriteLine("такое число не имеется в массиве");
+}
